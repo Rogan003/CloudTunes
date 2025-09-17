fdaa74ef534f048f6b141d1c2aeeea748f56521a
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-09-17 02:18:16 +0200

    add UploadContent request and response model (in aws-calls.ts)

--- src/users/models/aws-calls.ts
+++ src/users/models/aws-calls.ts
@@ -15,3 +15,31 @@ export interface CognitoTokens {
     idToken: string;
     refreshToken: string;
 }
+
+
+export interface UploadContentRequest {
+    filename: string;
+    filetype: string;
+    filesize: number;
+    title: string;
+    imageUrl?: string;
+    albumId?: string;
+    albumName?: string;
+    genres: string[];
+    artistIds: string[];
+}
+
+export interface UploadContentResponse {
+    contentId: string;
+    filename: string;
+    filetype: string;
+    filesize: number;
+    title: string;
+    imageUrl?: string;
+    albumId?: string;
+    albumName?: string;
+    genres: string[];
+    artistIds: string[];
+    createdAt: string;
+    updatedAt: string;
+}
